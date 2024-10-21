407fa87976868c9ee8e985f2d86fd70ca7ec3557
viktoryia133 <viktoryiakovalchik@mail.ru>
2024-10-21 15:17:51 +0300

    add option and argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+import { Command } from 'commander';
+
+const program = new Command();
 
 program
   .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
+  .version('0.8.0')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 program.parse();
